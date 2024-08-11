2b54831ba25381f2f4ede9997c3b22d9a99b1717
Lama Naser <Lamazahi40@gmail.com>
2024-08-11 23:31:10 +0300

    Update display.cpp
    Update display.cpp

--- Video_caoture/display.cpp
+++ Video_caoture/display.cpp
@@ -1,32 +1,32 @@
 
 void displayVideo() {
-	int frameIndex = 0;
+	int index = 0;
     	while (isRunning) {
-    		
+    		int frameIndex;
     		cv::Mat frame;
 		std::unique_lock<std::mutex> lock(bufferMutex);
 		
-        	if (!frameBuffer.empty() && frameBuffer.size() > frameIndex) {
-        		frame = frameBuffer.at(frameIndex);
-        		frameIndex++;
+        	if (!frameBuffer.empty() && frameBuffer.size() > index) {
+        		frame = frameBuffer.at(index).first.clone();
+        		frameIndex = frameBuffer.at(index).second;
+        		index++;
 		} else 
 			continue;
         	
         
         	// Print the frame index on the image
         	std::string text = "Display, ";
-        	text += "Frame: " + std::to_string(frameIndex++);
+        	text += "Frame: " + std::to_string(frameIndex);
         	cv::putText(frame, text, cv::Point(30, 30), cv::FONT_HERSHEY_SIMPLEX, 
         		1, cv::Scalar(255, 255, 255), 2);
 	
         	// Lock the mutex for frame1 before writing to it
         	{
-        	    std::unique_lock<std::mutex> lock(frame1Mutex);
-        	    frame1 = frame.clone();
+        	    std::unique_lock<std::mutex> lock(displayMutex);
+        	    display = frame.clone();
         	}
         	
         	
 		if (cv::waitKey(30) >= 0) break;
 	}
 }
-
