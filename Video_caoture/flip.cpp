dfcd61a7a2827bfd26d6963751f684d912b309b5
Lama Naser <Lamazahi40@gmail.com>
2024-08-11 23:32:05 +0300

    Update flip.cpp
    Update flip.cpp

--- Video_caoture/flip.cpp
+++ Video_caoture/flip.cpp
@@ -1,14 +1,15 @@
 void flipVideo() {
-	int frameIndex = 0;
+	int index = 0;
 	while (isRunning) {
-	
+		int frameIndex;
 		cv::Mat flippedFrame;
         	cv::Mat frame;
 		std::unique_lock<std::mutex> lock(bufferMutex);
 		
-        	if (!frameBuffer.empty() && frameBuffer.size() > frameIndex) {
-        		frame = frameBuffer.at(frameIndex);
-        		frameIndex++;
+        	if (!frameBuffer.empty() && frameBuffer.size() > index) {
+        		frame = frameBuffer.at(index).first.clone();
+        		frameIndex = frameBuffer.at(index).second;
+        		index++;
 		} else 
 			continue;
         	
@@ -17,15 +18,15 @@ void flipVideo() {
 		
 		// Print the frame index on the image
 		std::string text = "Flip, ";
-        	text += "Frame: " + std::to_string(frameIndex++);
+        	text += "Frame: " + std::to_string(frameIndex);
         	cv::putText(flippedFrame, text, cv::Point(30, 30), cv::FONT_HERSHEY_SIMPLEX, 
         		1, cv::Scalar(255, 255, 255), 2);
         	
         	
         	// Lock the mutex for frame2 before writing to it
         	{
-        	    std::unique_lock<std::mutex> lock(frame2Mutex);
-        	    frame2 = flippedFrame.clone();
+        	    std::unique_lock<std::mutex> lock(flipMutex);
+        	    flip = flippedFrame.clone();
         	}
         	
         	if (cv::waitKey(30) >= 0) break;
